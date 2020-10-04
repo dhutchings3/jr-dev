c460f5308f093b818f150b9d2c23b7422a24688e
Danielle Marie <dhutchings36@yahoo.com>
2020-10-04 16:38:20 -0500

    worked on styling for front-end

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,11 +1,19 @@
+import { Typography } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
 import React from 'react'
 
 export default function Job ({job}) {
     return (
-        <div className={'job'}>
-            {job.title}
-            {job.company}
-        </div>
+        <Paper className='job>'>
+            <div>
+                <Typography variant='h6'>{job.title}</Typography>
+                <Typography variant='h5'>{job.company}</Typography>
+                <Typography>{job.location}</Typography>
+            </div>
+            <div>
+                <Typography>{job.created_at.split(' ').slice(0,3).join(' ')}</Typography>
+            </div>
+        </Paper>
     )
 
 }
